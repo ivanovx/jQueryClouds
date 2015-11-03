5ed3ea2d5433b727f5d327a626922e72670fb660
Ivan Ivanov <slayer_word_11@outlook.com>
2015-11-03 20:46:05 +0200

    add old beta project

---
+++ js/app.js
@@ -0,0 +1,14 @@
+ $(document).ready(function() {
+                $("#far-clouds").pan({
+                    fps: 30,
+                    speed: 0.7,
+                    dir: "left",
+                    depth: 30
+                });
+                $("#near-clouds").pan({
+                    fps: 30,
+                    speed: 1,
+                    dir: "left",
+                    depth: 70
+                });
+            });
