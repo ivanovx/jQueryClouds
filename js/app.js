7b671675a4a77bcbdb2cf0add6977e9e5be4f8f0
Ivan Ivanov <slayer_word_11@outlook.com>
2015-11-10 20:42:41 +0200

    add new core

--- js/app.js
+++ js/app.js
@@ -2,14 +2,12 @@ $(document).ready(function () {
     $("#far-clouds").clouds({
 		fps: 30,
         speed: 0.7,
-        dir: "left",
-        depth: 30
+        dir: "left"
     });
     
 	$("#near-clouds").clouds({
 		fps: 30,
         speed: 1,
-        dir: "left",
-        depth: 70
+        dir: "left"
     });
 });
