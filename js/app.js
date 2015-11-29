569c68b87bfab8c4da38255eed9f281a42315d85
Ivan Ivanov <slayer_word_11@outlook.com>
2015-11-29 18:40:33 +0200

    add ver 1.0.5 and prepare 1.1.0

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
 $(document).ready(function () {
-    $("#far-clouds").pan({
+    $("#far-clouds").clouds({
 		fps: 30,
         speed: 0.7,
-        dir: "left",
-        depth: 30
+        dir: "left"
     });
     
-	$("#near-clouds").pan({
+	$("#near-clouds").clouds({
 		fps: 30,
         speed: 1,
-        dir: "left",
-        depth: 70
+        dir: "left"
     });
 });
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Todo
+
+In progress
+
+## Options
+* Version 1.0.3
+
+```
+$("element").pan({
+	fps: 30,
+    speed: 0.7,
+    dir: "left",
+    depth: 30
+});
+```
+
+* Version 1.0.4
+
+```
+$("element").clouds({
+	fps: 30,
+    speed: 0.7,
+    dir: "left",
+    depth: 30
+});
+```
